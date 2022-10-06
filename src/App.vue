c60292e18f4e444c5bbb51af6a98f55d21a588c2
Grinch3214 <scorpiomax90@gmail.com>
2022-10-07 00:46:33 +0300

    first commit/test code

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,101 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+		<div class="random" :class="{ active: isActive }">{{ firstStr }}</div>
+		<button :disabled='disabled' @click="onEventRandomize">123321</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+		firstRandonArray: [
+			'Страсная',
+			'Хладнокровная',
+			'Озорная',
+			'Романтичная',
+			'Горячая',
+			'Пьяная',
+			'Игривая',
+			'Веселая',
+			'Сексуальная',
+			'Любопытная',
+			'Шальная',
+			'Яростная',
+		],
+		secondRandonArray: [
+			'императрица',
+			'медсестричка',
+			'стюардесса',
+			'лисичка',
+			'принцесса',
+			'певица',
+			'балерина',
+			'ангелочек',
+			'волшебница',
+			'учительница',
+			'соблазнительница',
+			'пчелка',
+			'любовница',
+			'домохозяйка',
+			'барыня',
+			'королева',
+			'крестьянка',
+			'секретарша',
+			'директриса',
+			'нинзя',
+			'богиня',
+			'снегурочка',
+			'амазонка',
+			'спортсменка',
+			'куртизанка',
+			'фея',
+			'снежинка',
+			'стриптизерша',
+			'ведьма',
+			'иностранка',
+			'монашка',
+		],
+		trihdRandomArray: [
+			'на самокате',
+			'в дырявых носках',
+			'с бутылкой пивасика',
+			'с бутылкой спиртного',
+			'танцующая на шесте',
+			'на белом коне',
+			'с ядерным оружием',
+			'в сауне',
+			'в позе лотоса',
+			'с тазиком оливье',
+			'на розовом мотоцикле',
+		],
+		firstStr: '',
+		disabled: false,
+		isActive: false,
+	}),
+	methods: {
+		randomSpinner(arr) {
+			let random = Math.floor(Math.random() * arr.length);
+			console.log(random)
+			for (let i = 0; i < arr.length; i++) {
+				if (random === i) {
+					console.log(arr[i])
+					this.firstStr += ` ${arr[i]}`
+				}
+			}
+		},
+		onEventRandomize() {
+			this.firstStr = '';
+			this.disabled = true;
+			this.isActive = false;
+			this.randomSpinner(this.firstRandonArray);
+			this.randomSpinner(this.secondRandonArray);
+			this.randomSpinner(this.trihdRandomArray);
+			setTimeout(() => {this.disabled = false}, 1000);
+			setTimeout(() => {this.isActive = true}, 1000);
+		}
+	}
 }
 </script>
 
@@ -25,4 +108,14 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.random {
+	font-size: 60px;
+	opacity: 0;
+	&.active {
+		transition: all .5s ease;
+		opacity: 1;
+	}
+}
+
 </style>
