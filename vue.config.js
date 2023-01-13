b18ddfeccdefb57ed77601e7bbd5db6784a3f525
Max <scorpiomax90@gmail.com>
2023-01-14 01:37:32 +0200

    deploy dist on github

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-	publicPath: process.env.NODE_ENV === "production" ? "/fe-21-07/game" : "/",
+	publicPath: process.env.NODE_ENV === "production" ? "/slot-machine" : "/",
 	css: {
 		loaderOptions: {
 			sass: {
